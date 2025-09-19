43f1ae3af54ee2eb05bc5d50a42e7c7ef6fd9248
cortexinvader <cortexinvader@gmail.com>
2025-09-19 22:56:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.1.8",
-  updateZipUrl: "https://github.com/cortexinvader/potential-enigma/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
