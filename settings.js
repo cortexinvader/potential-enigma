fcf8452d9349482690a5e089513e8fa2b3954574
cortexinvader <cortexinvader@gmail.com>
2025-09-19 22:49:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,14 +4,14 @@ const settings = {
   botName: process.env.BOT_NAME || "Cortex Bot",
   botOwner: process.env.BOT_OWNER || 'SULEIMAN', // Creator name
   ownerNumber: process.env.OWNER_NUMBERS || '2348088941798', //Set your number here without + symbol, just add country code & number without any space
-  ownerNumbers: (process.env.OWNER_NUMBERS || '2348088941798').split(',').map(num => num.trim()), // Support multiple owners
+  // ownerNumbers: (process.env.OWNER_NUMBERS || '2348088941798').split(',').map(num => num.trim()), // Support multiple owners
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.1.8",
-  updateZipUrl: "https://github.com/cortexinvader/Cortexbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/cortexinvader/potential-enigma/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
